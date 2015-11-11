f832a13c0f15d439935235fe30e827313fed4403
Giancarlo <guerragiancarlo8@gmail.com>
2015-11-11 18:35:56 +0100

    finished module implementation. needs some cleanup

---
+++ caesar.js
@@ -0,0 +1,153 @@
+var MODULE = function(){
+
+	var shift = 6
+	return {
+
+		cipher: function(message) {
+
+			if(shift > 0 || shift < 0){
+				shift = shift*-1;
+			}
+			if(shift == null){
+
+				shift = 3
+			}
+
+
+			splitted = message.split('');
+			var step1;
+			var step2;
+
+			var charcode;
+
+			for(var i = 0; i<splitted.length; i++){
+
+				if(splitted[i] >= "A" && splitted[i] <= "Z"){
+
+					charcode = splitted[i].charCodeAt(0)
+
+					if((charcode - shift) > 122){
+
+						console.log((charcode - shift))
+						charcode = (charcode - shift) - 26
+					}
+					else if((charcode - shift) < 97){
+
+						charcode = (charcode - shift) + 26
+					}
+					
+					else{
+
+						charcode = charcode - shift
+					}
+					step1 = charcode
+					step2 = String.fromCharCode(step1);
+
+					splitted[i] = step2;
+				}
+
+				if(splitted[i] >= "a" && splitted[i] <= "z"){
+
+
+					charcode = splitted[i].charCodeAt(0)
+
+
+					if((charcode - shift) > 122){
+
+						
+						charcode = (charcode - shift) - 26
+					}
+					else if((charcode - shift) < 97){
+
+						charcode = (charcode - shift) + 26
+					}
+
+					else{
+
+						charcode = charcode - shift
+					}
+					step1 = charcode
+					step2 = String.fromCharCode(step1);
+
+					splitted[i] = step2;
+				}	
+			}
+			return splitted.join('')
+		},
+
+		decypher: function(message){
+
+			if(shift > 0 || shift < 0){
+				shift = shift*-1;
+			}
+			if(shift == null){
+
+				shift = 3
+			}
+
+
+			splitted = message.split('');
+			var step1;
+			var step2;
+
+			var charcode;
+
+			for(var i = 0; i<splitted.length; i++){
+
+				if(splitted[i] >= "A" && splitted[i] <= "Z"){
+
+					charcode = splitted[i].charCodeAt(0)
+
+					if((charcode - shift) > 122){
+
+						charcode = (charcode + shift) - 26
+					}
+					else if((charcode + shift) < 97){
+
+						charcode = (charcode + shift) + 26
+					}
+					
+					else{
+
+						charcode = charcode + shift
+					}
+					step1 = charcode
+					step2 = String.fromCharCode(step1);
+
+					splitted[i] = step2;
+				}
+
+				if(splitted[i] >= "a" && splitted[i] <= "z"){
+
+
+					charcode = splitted[i].charCodeAt(0)
+
+
+					if((charcode + shift) > 122){
+
+						
+						charcode = (charcode - shift) - 26
+					}
+					else if((charcode + shift) < 97){
+
+						charcode = (charcode + shift) + 26
+					}
+
+					else{
+
+						charcode = charcode + shift
+					}
+					step1 = charcode
+					step2 = String.fromCharCode(step1);
+
+					splitted[i] = step2;
+				}	
+			}
+			return splitted.join('')
+
+		}}
+
+}();
+
+//console.log(MODULE.cipher("Et tu, brute?"));
+//console.log(MODULE.decypher("kz za, hxazk?"));
